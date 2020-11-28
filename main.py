0366115603542e4d2a2e9b6157009a291cde295f
Lulin <kistssy+dev@gmail.com>
2020-11-29 01:01:22 +0900

    main: Refactor to simplify code
    필요없는 if 문과 필드 삭제

--- main.py
+++ main.py
@@ -20,106 +20,101 @@ class ShowVideo(QtCore.QObject):
         self.drag = False
         self.default_x, self.default_y, self.w, self.h = -1, -1, -1, -1
         self.blue, self.yellow = (255, 0, 0), (0, 255, 255)
-        self.first_frame = True
         self.buffer_frame = None
         self.motion_count = 0
         self.total_frame = 0
 
     @QtCore.pyqtSlot()
     def startVideo(self):
+        ret, frame = self.camera.read()
+
+        def onMouse(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                self.drag = True
+                self.default_x = x
+                self.default_y = y
+
+            elif event == cv2.EVENT_LBUTTONUP:
+                if self.drag:
+                    drag = False
+                    self.w = x - self.default_x
+                    self.h = y - self.default_y
+
+                    if self.w > 0 and self.h > 0:
+                        img_draw = frame.copy()
+                        cv2.rectangle(img_draw, (self.default_x, self.default_y), (x, y), self.yellow, 2)
+                        cv2.imshow('video', img_draw)
+
+                        roi_cols = self.default_y + self.h
+                        roi_rows = self.default_x + self.w
+
+                        # roi = frame[self.default_y:roi_cols, self.default_x:roi_rows]
+                        # threshhold = np.shape(np.ravel(roi))[0]
+                    print(self.default_x, self.default_y, self.w, self.h)
+
+        # shape[0] = 높이 , shape[1] = 너비
+        cv2.startWindowThread()
+        cv2.imshow('video', frame)
+        cv2.setMouseCallback("video", onMouse, param=frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
         while True:
-            if self.first_frame is True:
-                ret, frame = self.camera.read()
+            ret, image = self.camera.read()
 
-                def onMouse(event, x, y, flags, param):
-                    if event == cv2.EVENT_LBUTTONDOWN:
-                        self.drag = True
-                        self.default_x = x
-                        self.default_y = y
+            # 처리 로직
+            self.total_frame += 1
+            roi_cols = self.default_y + self.h
+            roi_rows = self.default_x + self.w
+            frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            if self.buffer_frame is None:
+                self.buffer_frame = frame[self.default_y:roi_cols, self.default_x:roi_rows]
 
-                    elif event == cv2.EVENT_LBUTTONUP:
-                        if self.drag:
-                            drag = False
-                            self.w = x - self.default_x
-                            self.h = y - self.default_y
+            roi_frame = frame[self.default_y:roi_cols, self.default_x:roi_rows]
+            buffer_frame_norm = 1 / (1 + np.exp(self.buffer_frame))  # SIGMOID
+            roi_frame_norm = 1 / (1 + np.exp(roi_frame))  # SIGMOID
+            subtract_frame = np.sqrt(np.sum((buffer_frame_norm - roi_frame_norm) ** 2))  # L2 DISTANCE
 
-                            if self.w > 0 and self.h > 0:
-                                img_draw = frame.copy()
-                                cv2.rectangle(img_draw, (self.default_x, self.default_y), (x, y), self.yellow, 2)
-                                cv2.imshow('video', img_draw)
+            if self.total_frame == 1:
+                buff_error = subtract_frame
 
-                                roi_cols = self.default_y + self.h
-                                roi_rows = self.default_x + self.w
+            # cv2.imshow('roi', self.buffer_Frame)
+            if subtract_frame > buff_error * 1.3:
 
-                                # roi = frame[self.default_y:roi_cols, self.default_x:roi_rows]
-                                # threshhold = np.shape(np.ravel(roi))[0]
-                            print(self.default_x, self.default_y, self.w, self.h)
+                self.buffer_frame = roi_frame
 
-                # shape[0] = 높이 , shape[1] = 너비
-                cv2.startWindowThread()
-                cv2.imshow('video', frame)
-                cv2.setMouseCallback("video", onMouse, param=frame)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                self.first_frame = False
+                # textBrowser에 로그 기록
+                now = time.strftime('%y%m%d_%H%M%S', time.localtime(time.time()))
+                textBrowser.append(now + ', count=' + str(self.motion_count))
 
             else:
-                ret, image = self.camera.read()
+                self.buffer_frame = roi_frame
 
-                # 처리 로직
-                self.total_frame += 1
-                roi_cols = self.default_y + self.h
-                roi_rows = self.default_x + self.w
-                frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                if self.buffer_frame is None:
-                    self.buffer_frame = frame[self.default_y:roi_cols, self.default_x:roi_rows]
+            # 이전 오차값과 현재 오차값이 +-5% 이상이면 모션 감지
+            buff_error = subtract_frame
+            bounding_box_frame = frame.copy()
+            output_frame = cv2.rectangle(bounding_box_frame, (self.default_x, self.default_y),
+                                         (self.default_x + self.w, self.default_y + self.h), (0, 255, 0),
+                                         thickness=3)
+            qt_image1 = QtGui.QImage(output_frame.data,
+                                     self.width,
+                                     self.height,
+                                     output_frame.strides[0],
+                                     QtGui.QImage.Format_Grayscale8)
 
-                roi_frame = frame[self.default_y:roi_cols, self.default_x:roi_rows]
-                buffer_frame_norm = 1 / (1 + np.exp(self.buffer_frame))  # SIGMOID
-                roi_frame_norm = 1 / (1 + np.exp(roi_frame))  # SIGMOID
-                subtract_frame = np.sqrt(np.sum((buffer_frame_norm - roi_frame_norm) ** 2))  # L2 DISTANCE
+            h, w = roi_frame.shape
+            roi_frame_cvt_mat = cv2.resize(roi_frame, (h, w))
 
-                if self.total_frame == 1:
-                    buff_error = subtract_frame
+            qt_image2 = QtGui.QImage(roi_frame_cvt_mat.data,
+                                     h,
+                                     w,
+                                     roi_frame_cvt_mat.strides[0],
+                                     QtGui.QImage.Format_Grayscale8)
 
-                # cv2.imshow('roi', self.buffer_Frame)
-                if subtract_frame > buff_error * 1.3:
+            self.VideoSignal1.emit(qt_image1)
+            self.VideoSignal2.emit(qt_image2)
 
-                    self.buffer_frame = roi_frame
-
-                    # textBrowser에 로그 기록
-                    now = time.strftime('%y%m%d_%H%M%S', time.localtime(time.time()))
-                    textBrowser.append(now + ', count=' + str(self.motion_count))
-
-                else:
-                    self.buffer_frame = roi_frame
-
-                # 이전 오차값과 현재 오차값이 +-5% 이상이면 모션 감지
-                buff_error = subtract_frame
-                bounding_box_frame = frame.copy()
-                output_frame = cv2.rectangle(bounding_box_frame, (self.default_x, self.default_y),
-                                             (self.default_x + self.w, self.default_y + self.h), (0, 255, 0),
-                                             thickness=3)
-                qt_image1 = QtGui.QImage(output_frame.data,
-                                         self.width,
-                                         self.height,
-                                         output_frame.strides[0],
-                                         QtGui.QImage.Format_Grayscale8)
-
-                h, w = roi_frame.shape
-                roi_frame_cvt_mat = cv2.resize(roi_frame, (h, w))
-
-                qt_image2 = QtGui.QImage(roi_frame_cvt_mat.data,
-                                         h,
-                                         w,
-                                         roi_frame_cvt_mat.strides[0],
-                                         QtGui.QImage.Format_Grayscale8)
-
-                self.VideoSignal1.emit(qt_image1)
-                self.VideoSignal2.emit(qt_image2)
-
-                self.motion_count += 1
+            self.motion_count += 1
 
             loop = QtCore.QEventLoop()
             QtCore.QTimer.singleShot(33, loop.quit)
