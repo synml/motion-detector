0051dee23c30b5d15f55b143177b4d176c91e16b
Lulin <kistssy+dev@gmail.com>
2020-12-07 19:14:40 +0900

    스레드 구현 중

--- designer_main.py
+++ designer_main.py
@@ -6,19 +6,37 @@ import cv2
 form_class = uic.loadUiType('main.ui')[0]
 
 
-class MainWindow(QtWidgets.QMainWindow, QtCore.QObject, form_class):
+class Camera(QtCore.QObject):
+    def __init__(self):
+        super(Camera, self).__init__()
+        self.cam = cv2.VideoCapture(0)
+
+    def start(self):
+        while True:
+            ret, img = self.cam.read()
+            if not ret:
+                print('camera read error')
+                return
+
+            h, w, c = img.shape
+            qimg = QtGui.QImage(img.data, w, h, w * c, QtGui.QImage.Format_RGB888)
+            pixmap = QtGui.QPixmap.fromImage(qimg)
+            self.label.setPixmap(pixmap)
+            self.label.resize(pixmap.width(), pixmap.height())
+            cv2.waitKey(60)
+
+
+class MainWindow(QtWidgets.QMainWindow, form_class):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-        camera = cv2.VideoCapture(1)
-        ret, img = camera.read()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        h, w, c = img.shape
-        qimg = QtGui.QImage(img.data, w, h, w * c, QtGui.QImage.Format_RGB888)
-        pixmap = QtGui.QPixmap.fromImage(qimg)
-        self.label.setPixmap(pixmap)
-        self.label.resize(pixmap.width(), pixmap.height())
+        self.thread = QtCore.QThread()
+        self.thread.start()
+        self.camera = Camera()
+        self.camera.moveToThread(self.thread)
+
+        self.startButton.clicked.connect(self.camera.start)
 
 
 if __name__ == "__main__":
