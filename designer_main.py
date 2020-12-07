206cf2af20d89f733d4ca11d65437484490d05aa
Lulin <kistssy+dev@gmail.com>
2020-12-07 18:54:21 +0900

    웹캠 프레임 받아오기 구현

--- designer_main.py
+++ designer_main.py
@@ -1,26 +1,28 @@
 import sys
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5 import uic
+import cv2
 
 form_class = uic.loadUiType('main.ui')[0]
 
 
-
-class MainWindow(QtWidgets.QMainWindow, form_class):
+class MainWindow(QtWidgets.QMainWindow, QtCore.QObject, form_class):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-        pixmap = QtGui.QPixmap('res/1.jpg')
+        camera = cv2.VideoCapture(1)
+        ret, img = camera.read()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        h, w, c = img.shape
+        qimg = QtGui.QImage(img.data, w, h, w * c, QtGui.QImage.Format_RGB888)
+        pixmap = QtGui.QPixmap.fromImage(qimg)
         self.label.setPixmap(pixmap)
         self.label.resize(pixmap.width(), pixmap.height())
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    thread = QtCore.QThread()
-    thread.start()
     win = MainWindow()
-    win.moveToThread(thread)
     win.show()
     app.exec_()
