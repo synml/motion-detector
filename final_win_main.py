9564e4e6b2ce0a07cfa1ce3ebb9d5bb414ffcabe
chansoopark98 <60956651+chansoopark98@users.noreply.github.com>
2021-01-20 13:56:02 +0900

    update

--- final_win_main.py
+++ final_win_main.py
@@ -37,83 +37,6 @@ setOptionDialogUi = uic.loadUiType(setOptionDialogUI)[0]
 infoDialogUi = uic.loadUiType(os.path.abspath(infoDialogUI))[0]
 
 
-class IPCamera:
-    def __init__(self, rtsp_url: str):
-
-        # 데이터 프로세스 전송 파이프
-
-
-        self.rtsp_url = rtsp_url
-        self.parent_conn, child_conn = mp.Pipe()
-        # load process
-        self.p = mp.Process(target=self.update, args=(child_conn, rtsp_url))
-        # start process
-        self.p.daemon = True
-        self.p.start()
-
-
-    def get_first_frame(self):
-        _, frame = cv2.VideoCapture(self.rtsp_url).read()
-        return frame
-
-    def end(self):
-        # 프로세스 종료 요청
-        self.parent_conn.send(2)
-
-    def update(self, conn, rtsp_url: str):
-        # load cam into separate process
-        cap = cv2.VideoCapture(rtsp_url)
-
-        run = True
-        while run:
-            # 버퍼에서 카메라 데이터 수신
-            cap.grab()
-
-            # 입력 데이터 수신
-            rec_dat = conn.recv()
-
-            if rec_dat == 1:
-                # 프레임 수신 완료했을 경우
-                ret, frame = cap.read()
-                conn.send(frame)
-
-            elif rec_dat == 3:
-                print("gpio 출력")
-
-
-
-
-
-            elif rec_dat == 2:
-                # 요청이 없는 경우
-                cap.release()
-                run = False
-
-        conn.close()
-
-
-
-    def get_frame(self, mode):
-        # 카메라 연결 프로세스에서 프레임 수신하는데 사용
-        # resize 값 50% 증가인 경우 1.5
-        if mode == "capture":
-            # send request
-            self.parent_conn.send(1)
-            frame = self.parent_conn.recv()
-
-
-            # reset request
-            self.parent_conn.send(0)
-
-            return frame
-
-            # resize if needed
-        elif mode == "signal":
-            self.parent_conn.send(3)
-            
-
-            # reset request
-            self.parent_conn.send(0)
 
 
 
@@ -294,21 +217,12 @@ class MotionDetector(QtCore.QObject):
                 # 일반 감지 상태
                 if self.idleMode == False:
                     if subtract_frame > self.buffError * self.threshold:
-                        self.write_log('이상 감지')
+                        self.ip_camera.get_frame("signal")
                         self.idleMode = True
-                        self.idleInitTime = time.time()
+
+                else: #self.idleMode == True:
                         self.ip_camera.get_frame("signal")
 
-                if self.idleMode == True:
-                        win.statusLabel.setText("유휴 상태")
-                        win.idleTimeLcd.display(
-                            (self.idleInitTime + self.idleTime) - time.time())
-
-                        if self.idleInitTime + self.idleTime <= time.time():
-                            self.idleMode = False  # 유휴상태 해제
-
-                            win.statusLabel.setText("일반 감지 상태")
-                            win.idleTimeLcd.display(0)
 
                 self.buffer_frame = self.roi_frame
 
@@ -327,6 +241,93 @@ class MotionDetector(QtCore.QObject):
             self.label.setPixmap(pixmap)
 
 
+class IPCamera():
+    def __init__(self, rtsp_url: str):
+        super(IPCamera, self).__init__()
+        # 데이터 프로세스 전송 파이프
+
+        self.rtsp_url = rtsp_url
+        self.parent_conn, child_conn = mp.Pipe()
+        # load process
+        self.p = mp.Process(target=self.update, args=(child_conn, rtsp_url))
+        # start process
+        self.p.daemon = True
+        self.p.start()
+
+    def get_first_frame(self):
+        _, frame = cv2.VideoCapture(self.rtsp_url).read()
+        return frame
+
+    def end(self):
+        # 프로세스 종료 요청
+        self.parent_conn.send(2)
+
+    def update(self, conn, rtsp_url: str):
+        # load cam into separate process
+        cap = cv2.VideoCapture(rtsp_url)
+
+        run = True
+        while run:
+            # 버퍼에서 카메라 데이터 수신
+            cap.grab()
+
+            # 입력 데이터 수신
+            rec_dat = conn.recv()
+
+            if rec_dat == 1:
+                # 프레임 수신 완료했을 경우
+                ret, frame = cap.read()
+                conn.send(frame)
+
+            elif rec_dat == 3:
+
+                if win.motionDetector.idleMode == False:
+
+                        win.motionDetector.write_log('이상 감지')
+                        win.motionDetector.idleMode = True
+                        win.motionDetector.idleInitTime = time.time()
+
+
+                if win.motionDetector.idleMode == True:
+                        win.statusLabel.setText("유휴 상태")
+                        win.idleTimeLcd.display(
+                            (win.motionDetector.idleInitTime + win.motionDetector.idleTime) - time.time())
+
+                        if win.motionDetector.idleInitTime + win.motionDetector.idleTime <= time.time():
+                            win.motionDetector.idleMode = False  # 유휴상태 해제
+
+                            win.statusLabel.setText("일반 감지 상태")
+                            win.idleTimeLcd.display(0)
+
+
+
+
+            elif rec_dat == 2:
+                # 요청이 없는 경우
+                cap.release()
+                run = False
+
+        conn.close()
+
+    def get_frame(self, mode):
+        # 카메라 연결 프로세스에서 프레임 수신하는데 사용
+        # resize 값 50% 증가인 경우 1.5
+        if mode == "capture":
+            # send request
+            self.parent_conn.send(1)
+            frame = self.parent_conn.recv()
+
+            # reset request
+            self.parent_conn.send(0)
+
+            return frame
+
+            # resize if needed
+        elif mode == "signal":
+            self.parent_conn.send(3)
+
+            # reset request
+            self.parent_conn.send(0)
 
 
 class InfoDialog(QtWidgets.QDialog, infoDialogUi):
